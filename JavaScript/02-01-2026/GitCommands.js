6b9df7bf4383769dc761b74a2cd6be64ab9ac0f8
Abdul Rehaman <abdul.rehaman@nybinfotech.com>
2026-01-30 16:55:51 +0530

    Webdevelopment

---
+++ JavaScript/02-01-2026/GitCommands.js
@@ -0,0 +1,18 @@
+
+// Git and github concepts
+//1.working directory 
+//2. staging area
+//3.local repository
+//4.central repository
+     
+
+
+// Git commands
+//1.git add
+//3.git commit
+//4.git push
+//5.git status
+//6.git log
+//7.git branch
+//8.git switch
+//9.git init
---
+++ JavaScript/06-01-2026/GitCommands.js
@@ -0,0 +1,34 @@
+// restoring commamds
+
+// git restore -- staged
+
+// (it moves to staging area to working directory)
+
+// git reset --soft Head-1
+
+// (it moves from local repository to stagging area)
+
+// git reset --hard Head-1
+
+// (it moves from local repository to working directory)
+
+// git revert id
+
+// git stash 
+
+
+let a=10;
+let b=20;
+console.log(a+b)
+
+
+function abdul(){
+    let ab=20;
+    let bc=20;
+    console.log(ab+bc)
+}
+
+
+abdul()
+
+
