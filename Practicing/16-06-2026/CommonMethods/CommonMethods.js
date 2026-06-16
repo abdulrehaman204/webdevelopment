7161edc89cc40bd5f94d0fc367db6212afde0f2d
Abdul Rehaman <abdul.rehaman@nybinfotech.com>
2026-06-16 18:53:48 +0530

    webdevelopment

---
+++ Practicing/16-06-2026/CommonMethods/CommonMethods.js
@@ -0,0 +1,29 @@
+// common methods
+
+// parsint
+console.log(parseInt("123"));      
+console.log(parseInt("123.45"));   
+console.log(parseInt("abc"));
+
+// parsefloat
+console.log(parseFloat("12.33"))
+console.log(parseFloat("2.44px"))
+
+// json.stringfy
+const obj2 = { name: "Abdul", age: 23 };
+const str = JSON.stringify(obj2);
+console.log(str);
+
+// json.parse
+const str1 = '{"name":"Abdul","age":25}';
+const obj3 = JSON.parse(str1);
+console.log(obj3.name); 
+
+// tostring
+let n = 10;
+console.log(n.toString())
+
+// tofixed
+let num1 = 12.345;
+console.log(num1.toFixed(2)); // "12.35"
+
---
+++ Practicing/16-06-2026/OutputMethods/Output.js
@@ -0,0 +1,30 @@
+// output methods
+
+// inner.HTML
+let d= document.getElementById("gfg")
+d.innerHTML="Hello Abdul"
+d.style.color="green"
+d.style.textAlign="Center"
+
+// console.log
+console.log("Hello, Mr.Abdul")
+
+// document.write
+document.write("hello")
+
+// window.alert
+window.alert("Hello Abdul")
+
+// window.print
+// window.print()
+
+// // window.prompt
+  let userInput = window.prompt("Please Enter your Input");
+        if (userInput !== null) {
+            window.alert("Hello, " + userInput + "!");
+        } else {
+            window.alert("You clicked Cancel or closed the prompt.");
+        }
+
+
+
